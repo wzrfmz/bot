90c22ffbf271abcfab5424c760140e8e3b589dac
wzrfmz <yomiko.bot@gmail.com>
2024-07-29 20:03:45 +0000

    Pending changes exported from your codespace

---
+++ BOTWZRFMZ/__main__.py
@@ -0,0 +1,8 @@
+from pyrogram import idle
+import asyncio 
+from pyrogram.errors import RCPError
+from uvloop import install 
+
+from BOTWZRFMZ import BOTWZRFMZ, BOTWZRFMZ, BOTWZRFMZ, LOGGER #KalokErorKesini
+from BOTWZRFMZ.config import LOGS
+from BOTWZRFMZ.core.functions.expired import expired_date, rebot
